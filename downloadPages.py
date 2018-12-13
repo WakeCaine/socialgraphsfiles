2193096470387294cff0a51d10867be60736b908
Dariusz "Caine <WakeCaine@users.noreply.github.com>
2018-12-13 09:17:14 +0100

    Update downloadPages.py

--- downloadPages.py
+++ downloadPages.py
@@ -20,7 +20,7 @@ for years in range(1):
             for l in range(10):
                 month = str(j + 1) if (j + 1) > 9 else ("0" + str(j + 1))
                 day = str(k + 1) if (k + 1) > 9 else ("0" + str(k + 1))
-                cmd = "curl -u wakecaine1:d32914c5efad243710c300019ccdcab61a76e2a5 -X GET 'https://api.github.com/search/repositories?q=game+created:%s-%s-%s&per_page=100&page=%s' > %s_%s_%s_%s.json" % (year_start,month,day, l+1, year_start,month,day,l+1)
+                cmd = "curl -u username:token -X GET 'https://api.github.com/search/repositories?q=game+created:%s-%s-%s&per_page=100&page=%s' > %s_%s_%s_%s.json" % (year_start,month,day, l+1, year_start,month,day,l+1)
                 print(cmd)
                 results = subprocess.run(cmd, shell=True, universal_newlines=True, check=True)
                 fileName = "%s_%s_%s_%s.json" % (year_start,month,day,l+1)
